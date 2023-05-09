8a5bb0493efa0f7101e25cec2a40f4e3db9f39ae
chris-lock2 <chris.lock1998@gmail.com>
2023-05-08 23:33:30 -0300

    Atualizando dependencias

---
+++ ds/src/test/java/com/estudospring/ds/DsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudospring.ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
